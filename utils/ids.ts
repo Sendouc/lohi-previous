337a02c9056052794814686038d9a4242e559fe7
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-02-10 11:52:36 +0200

    plus two pings

--- utils/ids.ts
+++ utils/ids.ts
@@ -5,14 +5,16 @@ const ids = {
   roles: {
     sroLfg: "749589703371653158",
     sroCoach: "807258345223356486",
+    plusTwoPings: "678602424314888224",
   },
   channels: {
-    // Salmon Run
-    bot: ["746130457455886337"],
+    // Salmon Run // Plus Server
+    bot: ["746130457455886337", "808995745768013854"],
     sroLfg: "745036737943699546",
   },
   guilds: {
     sro: "737359708276654121",
+    plusServer: "351828612208525333",
   },
 } as const;
 
--- utils/roleManagerIdMap.ts
+++ utils/roleManagerIdMap.ts
@@ -5,6 +5,9 @@ const roleManagerIdMap: Record<string, Record<string, string>> = {
     lfg: ids.roles.sroLfg,
     coach: ids.roles.sroCoach,
   },
+  [ids.guilds.plusServer]: {
+    pings: ids.roles.plusTwoPings,
+  },
 } as const;
 
 export default roleManagerIdMap;
